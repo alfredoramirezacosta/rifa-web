63907ddc843524b8de5159bd30ebbbfd3e8634c5
alfredoramirezacosta <77932694+alfredoramirezacosta@users.noreply.github.com>
2026-02-27 20:29:19 -0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,11 @@
-Console.log("Admin listo")
+function login(){
+
+ let clave = document.getElementById("clave").value;
+
+ if(clave === "rifa2026"){
+     document.getElementById("panel").style.display="block";
+ }else{
+     alert("Contraseña incorrecta");
+ }
+
+}
