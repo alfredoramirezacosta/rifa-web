3f7338155a9f64c58b6dfe25c570a554bc8f49bf
alfredoramirezacosta <77932694+alfredoramirezacosta@users.noreply.github.com>
2026-02-27 20:45:36 -0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -4,8 +4,37 @@ function login(){
 
  if(clave === "rifa2026"){
      document.getElementById("panel").style.display="block";
+     cargarParticipantes();
  }else{
      alert("Contraseña incorrecta");
  }
+
+}
+
+ function cargarParticipantes(){
+
+ db.collection("participantes")
+ .orderBy("numero")
+ .onSnapshot((snapshot)=>{
+
+   let tabla = document.getElementById("lista");
+   tabla.innerHTML="";
+
+   snapshot.forEach(doc=>{
+
+     let data = doc.data();
+
+     tabla.innerHTML += `
+       <tr>
+         <td>${data.nombre}</td>
+         <td>${data.telefono}</td>
+         <td>#${data.numero}</td>
+       </tr>
+     `;
+   });
+
+ });
+
+}
 
 }
